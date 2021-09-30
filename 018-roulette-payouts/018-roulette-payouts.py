781fc9a11625817357eba04ea5e281bd1106af66
JustProsa <90556911+JustProsa@users.noreply.github.com>
2021-10-01 00:09:05 +0200

    Update 018-roulette-payouts.py

--- 018-roulette-payouts/018-roulette-payouts.py
+++ 018-roulette-payouts/018-roulette-payouts.py
@@ -23,6 +23,7 @@
 import random
 
 # Starting conditions
+numbers = [00, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 red_numbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black_numbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 green_numbers = [00, 0]
@@ -39,32 +40,36 @@ print("\nAnd now enter your bet!")
 bet = input()
 
 # Generate number
-num = int(random.randint(00, 36))
-print(num)
+num = int(random.randint(0, 37))
+print(numbers[num])
 
 
 
 # Conditions
-if num in red_numbers:
-    single_number = "Pay red"
-elif num in black_numbers:
-    single_number = "Pay black"
-elif num in green_numbers:
-    single_number = "Pay green"
+if numbers[num] in red_numbers:
+    color_number = "Pay red"
+elif numbers[num] in black_numbers:
+    color_number = "Pay black"
 
-if num % 2 == 0:
+if numbers[num] % 2 == 0:
     odd_or_even = "Pay Even"
 else:
-    odd_or_even = "Pay Even"
+    odd_or_even = "Pay Odd"
 
-if num <= 18:
+if numbers[num] <= 18:
     range_number = "Pay 1 to 18"
 else:
     range_number = "Pay 19 to 36"
 
+if numbers[num] == 00 or numbers[num] == 0:
+    color_number = ""
+    odd_or_even = ""
+    range_number = ""
+
+
 # Results
-print("Pay " + str(num))
-print(single_number)
+print("Pay " + str(numbers[num]))
+print(color_number)
 print(odd_or_even)
 print(range_number)
 
