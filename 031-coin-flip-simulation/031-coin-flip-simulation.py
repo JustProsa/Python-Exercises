61650bb0d13aa3c9cec5c963f10af61532c3b422
JustProsa <90556911+JustProsa@users.noreply.github.com>
2021-10-08 19:51:17 +0200

    Update 031-coin-flip-simulation.py

--- 031-coin-flip-simulation/031-coin-flip-simulation.py
+++ 031-coin-flip-simulation/031-coin-flip-simulation.py
@@ -14,18 +14,175 @@
 import random
 
 
-def head_or_tail():
-    h_or_t = []
+def head_or_tail1():
+    h_or_t1 = []
     while True:
-        h_or_t.append(random.randint(0, 1))
-        if h_or_t[len(h_or_t) - 1] == 1 and h_or_t[len(h_or_t) - 2] == 1 and h_or_t[len(h_or_t) - 3] == 1:
-            break
-    for num in h_or_t:
-        if num == 0:
-            print("H", end= " ")
-        elif num == 1:
-            print("T", end= " ")
+        h_or_t1.append(random.randint(0, 1))
+        if len(h_or_t1) >= 3:
+            if h_or_t1[len(h_or_t1) - 1] == 1 and h_or_t1[len(h_or_t1) - 2] == 1 and h_or_t1[len(h_or_t1) - 3] == 1 or h_or_t1[len(h_or_t1) - 1] == 0 and h_or_t1[len(h_or_t1) - 2] == 0 and h_or_t1[len(h_or_t1) - 3] == 0:
+                for num in h_or_t1:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t1)) + " flips)")
+                break    
+            else:
+                h_or_t1.append(random.randint(0, 1))
 
-    print("(" + str(len(h_or_t)) + " flips)")
-head_or_tail()
+def head_or_tail2():
+    head_or_tail2 = []
+    while True:
+        head_or_tail2.append(random.randint(0, 1))
+        if len(head_or_tail2) >= 3:
+            if head_or_tail2[len(head_or_tail2) - 1] == 1 and head_or_tail2[len(head_or_tail2) - 2] == 1 and head_or_tail2[len(head_or_tail2) - 3] == 1 or head_or_tail2[len(head_or_tail2) - 1] == 0 and head_or_tail2[len(head_or_tail2) - 2] == 0 and head_or_tail2[len(head_or_tail2) - 3] == 0:
+                for num in head_or_tail2:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(head_or_tail2)) + " flips)")
+                break    
+            else:
+                head_or_tail2.append(random.randint(0, 1))
+
+def head_or_tail3():
+    h_or_t3 = []
+    while True:
+        h_or_t3.append(random.randint(0, 1))
+        if len(h_or_t3) >= 3:
+            if h_or_t3[len(h_or_t3) - 1] == 1 and h_or_t3[len(h_or_t3) - 2] == 1 and h_or_t3[len(h_or_t3) - 3] == 1 or h_or_t3[len(h_or_t3) - 1] == 0 and h_or_t3[len(h_or_t3) - 2] == 0 and h_or_t3[len(h_or_t3) - 3] == 0:
+                for num in h_or_t3:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t3)) + " flips)")
+                break    
+            else:
+                h_or_t3.append(random.randint(0, 1))
+
+def head_or_tail4():
+    h_or_t4 = []
+    while True:
+        h_or_t4.append(random.randint(0, 1))
+        if len(h_or_t4) >= 3:
+            if h_or_t4[len(h_or_t4) - 1] == 1 and h_or_t4[len(h_or_t4) - 2] == 1 and h_or_t4[len(h_or_t4) - 3] == 1 or h_or_t4[len(h_or_t4) - 1] == 0 and h_or_t4[len(h_or_t4) - 2] == 0 and h_or_t4[len(h_or_t4) - 3] == 0:
+                for num in h_or_t4:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t4)) + " flips)")
+                break    
+            else:
+                h_or_t4.append(random.randint(0, 1))
+
+def head_or_tail5():
+    h_or_t5 = []
+    while True:
+        h_or_t5.append(random.randint(0, 1))
+        if len(h_or_t5) >= 3:
+            if h_or_t5[len(h_or_t5) - 1] == 1 and h_or_t5[len(h_or_t5) - 2] == 1 and h_or_t5[len(h_or_t5) - 3] == 1 or h_or_t5[len(h_or_t5) - 1] == 0 and h_or_t5[len(h_or_t5) - 2] == 0 and h_or_t5[len(h_or_t5) - 3] == 0:
+                for num in h_or_t5:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t5)) + " flips)")
+                break    
+            else:
+                h_or_t5.append(random.randint(0, 1))
+
+def head_or_tail6():
+    h_or_t6 = []
+    while True:
+        h_or_t6.append(random.randint(0, 1))
+        if len(h_or_t6) >= 3:
+            if h_or_t6[len(h_or_t6) - 1] == 1 and h_or_t6[len(h_or_t6) - 2] == 1 and h_or_t6[len(h_or_t6) - 3] == 1 or h_or_t6[len(h_or_t6) - 1] == 0 and h_or_t6[len(h_or_t6) - 2] == 0 and h_or_t6[len(h_or_t6) - 3] == 0:
+                for num in h_or_t6:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t6)) + " flips)")
+                break    
+            else:
+                h_or_t6.append(random.randint(0, 1))
+
+def head_or_tail7():
+    h_or_t7 = []
+    while True:
+        h_or_t7.append(random.randint(0, 1))
+        if len(h_or_t7) >= 3:
+            if h_or_t7[len(h_or_t7) - 1] == 1 and h_or_t7[len(h_or_t7) - 2] == 1 and h_or_t7[len(h_or_t7) - 3] == 1 or h_or_t7[len(h_or_t7) - 1] == 0 and h_or_t7[len(h_or_t7) - 2] == 0 and h_or_t7[len(h_or_t7) - 3] == 0:
+                for num in h_or_t7:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t7)) + " flips)")
+                break    
+            else:
+                h_or_t7.append(random.randint(0, 1))
+
+def head_or_tail8():
+    h_or_t8 = []
+    while True:
+        h_or_t8.append(random.randint(0, 1))
+        if len(h_or_t8) >= 3:
+            if h_or_t8[len(h_or_t8) - 1] == 1 and h_or_t8[len(h_or_t8) - 2] == 1 and h_or_t8[len(h_or_t8) - 3] == 1 or h_or_t8[len(h_or_t8) - 1] == 0 and h_or_t8[len(h_or_t8) - 2] == 0 and h_or_t8[len(h_or_t8) - 3] == 0:
+                for num in h_or_t8:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t8)) + " flips)")
+                break    
+            else:
+                h_or_t8.append(random.randint(0, 1))
+
+def head_or_tail9():
+    h_or_t9 = []
+    while True:
+        h_or_t9.append(random.randint(0, 1))
+        if len(h_or_t9) >= 3:
+            if h_or_t9[len(h_or_t9) - 1] == 1 and h_or_t9[len(h_or_t9) - 2] == 1 and h_or_t9[len(h_or_t9) - 3] == 1 or h_or_t9[len(h_or_t9) - 1] == 0 and h_or_t9[len(h_or_t9) - 2] == 0 and h_or_t9[len(h_or_t9) - 3] == 0:
+                for num in h_or_t9:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t9)) + " flips)")
+                break    
+            else:
+                h_or_t9.append(random.randint(0, 1))
+
+def head_or_tail10():
+    h_or_t10 = []
+    while True:
+        h_or_t10.append(random.randint(0, 1))
+        if len(h_or_t10) >= 3:
+            if h_or_t10[len(h_or_t10) - 1] == 1 and h_or_t10[len(h_or_t10) - 2] == 1 and h_or_t10[len(h_or_t10) - 3] == 1 or h_or_t10[len(h_or_t10) - 1] == 0 and h_or_t10[len(h_or_t10) - 2] == 0 and h_or_t10[len(h_or_t10) - 3] == 0:
+                for num in h_or_t10:
+                    if num == 0:
+                        print("H", end= " ")
+                    elif num == 1:
+                        print("T", end= " ")
+                print("(" + str(len(h_or_t10)) + " flips)")
+                break    
+            else:
+                h_or_t10.append(random.randint(0, 1))
+
+head_or_tail1()
+head_or_tail2()
+head_or_tail3()
+head_or_tail4()
+head_or_tail5()
+head_or_tail6()
+head_or_tail7()
+head_or_tail8()
+head_or_tail9()
+head_or_tail10()
+
 
