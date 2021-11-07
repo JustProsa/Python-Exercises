b38a207df2279c27af7875fdeeccb60f7a0b079b
JustProsa <90556911+JustProsa@users.noreply.github.com>
2021-11-07 19:02:11 +0100

    Update 024-create-a-bingo-card.py

--- M2/024-create-a-bingo-card/024-create-a-bingo-card.py
+++ M2/024-create-a-bingo-card/024-create-a-bingo-card.py
@@ -19,13 +19,15 @@ import random
 
 bingo = {"B":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "I":[16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], "N":[31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45], "G":[46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60], "O":[61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 61, 72, 73, 74, 75]}
 
-keys_list = list(bingo.keys())
-values_list = list(bingo.values())
+bingo_card = {}
 
+def generateCard(c):
+    c["B"] = random.sample(range(1, 15), 5)
+    c["I"] = random.sample(range(16, 30), 5)
+    c["N"] = random.sample(range(31, 45), 5)
+    c["G"] = random.sample(range(46, 60), 5)
+    c["O"] = random.sample(range(61, 75), 5)
 
-bingo_card = random.randint(0, len(keys_list))
-bingo_number = random.randint(0, 15)
+    return c
 
-print("BINGO CARD:")
-print(str(keys_list[bingo_card]), end= " ")
-print(values_list[bingo_card][bingo_number])
+print(generateCard(bingo_card))
